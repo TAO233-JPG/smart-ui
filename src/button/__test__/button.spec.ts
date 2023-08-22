c782b56817d9d3d37fd5ba5cc6062d6e40b22d36
TAO233-JPG <1040581451@qq.com>
2023-08-22 23:09:44 +0800

    test: 修改button组件

--- src/button/__test__/button.spec.ts
+++ src/button/__test__/button.spec.ts
@@ -1,86 +1,75 @@
-import Button from "../index";
-
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
+import SButton, { ButtonProps } from "../index";
+
 // 测试分组
-describe("Button", () => {
-  // mount
-  test("mount  @vue/test-utils", () => {
-    // @vue/test-utils
-    const wrapper = shallowMount(Button, {
+describe("SButton", () => {
+  test("base props", () => {
+    const content = "connnnent";
+    const wrapper = shallowMount(SButton, {
       slots: {
-        default: "Button",
+        default: content,
       },
     });
-
     // 断言
-    expect(wrapper.text()).toBe("Button");
+    expect(wrapper.text()).toBe(content);
+    expect(wrapper.classes()).toContain("s-button");
   });
-});
 
-describe("type", () => {
-  test("default", () => {
-    const wrapper = shallowMount(Button, {
-      slots: {
-        default: "Button",
-      },
-    });
-    expect(
-      wrapper
-        .classes()
-        .map((v) => v.replace("\n", ""))
-        .includes("bg-blue-500"),
-    ).toBe(true);
-  });
-  test("danger", () => {
-    const wrapper = shallowMount(Button, {
-      slots: {
-        default: "Button",
-      },
-      props: {
-        type: "danger",
-      },
-    });
-    expect(
-      wrapper
-        .classes()
-        .map((v) => v.replace("\n", ""))
-        .includes("bg-red-500"),
-    ).toBe(true);
-  });
-});
+  test("type", () => {
+    const button_types: ButtonProps["type"][] = [
+      "default",
+      "primary",
+      "danger",
+      "info",
+      "success",
+      "warning",
+    ];
 
-describe("plain", () => {
-  test("defalut", () => {
-    const wrapper = shallowMount(Button, {
-      slots: {
-        default: "Button",
-      },
-      props: {},
+    button_types.forEach((type_item) => {
+      const wrapper = shallowMount(SButton, {
+        props: { type: type_item },
+      });
+      expect(wrapper.classes()).toContain(`s-button__${type_item}`);
     });
-
-    expect(
-      wrapper
-        .classes()
-        .map((v) => v.replace("\n", ""))
-        .includes("500"),
-    );
   });
+
+  test("size", () => {
+    const button_sizes: ButtonProps["size"][] = ["large", "middle", "small"];
+
+    button_sizes.forEach((size_item) => {
+      const wrapper = shallowMount(SButton, {
+        props: { size: size_item },
+      });
+      expect(wrapper.classes()).toContain(`s-button__${size_item}`);
+    });
+  });
+
+  test("round", () => {
+    const wrapper = shallowMount(SButton, {
+      props: { round: true },
+    });
+    expect(wrapper.classes()).toContain(`s-button__round`);
+  });
+
   test("plain", () => {
-    const wrapper = shallowMount(Button, {
-      slots: {
-        default: "Button",
-      },
-      props: {
-        plain: true,
-      },
+    const wrapper = shallowMount(SButton, {
+      props: { plain: true },
     });
+    expect(wrapper.classes()).toContain(`s-button__plain`);
+  });
 
-    expect(
-      wrapper
-        .classes()
-        .map((v) => v.replace("\n", ""))
-        .includes("400"),
-    );
+  test("circle", () => {
+    const wrapper = shallowMount(SButton, {
+      props: { circle: true },
+    });
+    expect(wrapper.classes()).toContain(`s-button__circle`);
+  });
+
+  test("disabled", () => {
+    const wrapper = shallowMount(SButton, {
+      props: { disabled: true },
+    });
+    expect(wrapper.attributes("disabled")).toBeDefined()
   });
 });
